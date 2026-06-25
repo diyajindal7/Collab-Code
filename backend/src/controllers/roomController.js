d6a27644319faa07650fd6866f804c317588601b
Diya Jindal <diyajindal265@gmail.com>
2026-06-26 00:37:05 +0530

    Phase 2 Complete - Room Management APIs

--- backend/src/controllers/roomController.js
+++ backend/src/controllers/roomController.js
@@ -58,6 +58,103 @@ const createRoom = async (req, res) => {
   }
 };
 
+const joinRoom = async (req, res) => {
+    try {
+
+        const { roomCode } = req.body;
+
+        if (!roomCode) {
+            return res.status(400).json({
+                success: false,
+                message: "Room code is required"
+            });
+        }
+
+        const userId = req.user.userId;
+
+        // Find room
+        const room = await Room.findOne({ roomCode });
+
+        if (!room) {
+            return res.status(404).json({
+                success: false,
+                message: "Room not found"
+            });
+        }
+
+        // Check if already joined
+        const alreadyJoined = room.participants.some(
+    participant => participant.toString() === userId
+);
+
+if (alreadyJoined) {
+    return res.status(400).json({
+        success: false,
+        message: "Already joined this room"
+    });
+}
+
+        // Add user to room
+        room.participants.push(userId);
+
+        await room.save();
+
+        // Update user
+        await User.findByIdAndUpdate(userId, {
+            $push: {
+                joinedRooms: room._id
+            }
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "Joined room successfully",
+            room
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+
+    }
+};
+
+
+const getMyRooms = async (req, res) => {
+    try {
+
+        const userId = req.user.userId;
+
+        const user = await User.findById(userId)
+            .populate("createdRooms")
+            .populate("joinedRooms");
+
+        res.status(200).json({
+            success: true,
+            createdRooms: user.createdRooms,
+            joinedRooms: user.joinedRooms
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+
+    }
+};
+
+
 module.exports = {
-  createRoom
+    createRoom,
+    joinRoom,
+    getMyRooms
 };
--- backend/src/routes/roomRoutes.js
+++ backend/src/routes/roomRoutes.js
@@ -4,9 +4,16 @@ const router = express.Router();
 const protect = require("../middleware/auth");
 
 const {
-  createRoom
+    createRoom,
+    joinRoom,
+    getMyRooms
 } = require("../controllers/roomController");
 
+
 router.post("/create", protect, createRoom);
+
+router.post("/join", protect, joinRoom);
+
+router.get("/my-rooms", protect, getMyRooms);
 
 module.exports = router;
